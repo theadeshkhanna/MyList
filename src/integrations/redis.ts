0fd8adb637f3c3827e2711885cdf37012a1db2ef
Adesh Khanna <adeshkhanna271@gmail.com>
2025-05-26 20:17:49 +0530

    modified: redis implementation

--- src/integrations/redis.ts
+++ src/integrations/redis.ts
@@ -1,7 +1,8 @@
 import Redis from "ioredis";
-import { REDIS_HOST, REDIS_PORT } from "../secrets";
+import { REDIS_URI } from "../secrets";
 
-export const redisClient = new Redis({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
+export const redisClient = new Redis(REDIS_URI, {
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
--- src/secrets.ts
+++ src/secrets.ts
@@ -5,5 +5,4 @@ dotenv.config({ path: ".env" });
 export const PORT = process.env.PORT;
 export const JWT_SECRET = process.env.JWT_SECRET!;
 
-export const REDIS_HOST = process.env.REDIS_HOST!;
-export const REDIS_PORT = parseInt(process.env.REDIS_PORT as string);
+export const REDIS_URI = process.env.REDIS_URI!;
