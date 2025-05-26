ec60f420d27abb52917f69f3b507b15826cd0e6d
Adesh Khanna <adeshkhanna271@gmail.com>
2025-05-26 20:41:45 +0530

    modified: base url

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
 import app from "./app";
+import { Request, Response } from "express";
 import { PORT } from "./secrets";
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    name: "Adesh Khanna",
+    product: "MyList",
+    howToUse: "Refer github readme",
+    APIStatus: "Working",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is working`);
 });
-
-//**
-// 1. make all tables and relations ✅
-// 2. crud of only Lists ✅
-// 3. seeders ✅
-// 4. every method in try and catch in controllers ✅
-// 5. singleton service for every model ✅
-// 6. commenting for better understanding
-// 6.5. zod valdations ✅
-// 7. proper error handling
-// 7.5 add middlewares ✅
-// 8. dockerization
-// 8.5. remove chatgpt console logs ✅
-// 9. write tests ✅
-// 9.5 user service ✅
-// 10. think of using redis for fast My List access
-//*
